6b6e7d486e86c6d4a175587babbec9873c4c31e9
Ruben Menino <ruben.menino@ua.pt>
2023-02-18 14:36:32 +0000

    add some content

---
+++ test.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
