242cb1048b4f4bd759b9cf09e20158b96ff8d5d3
Ruben Menino <ruben.menino@ua.pt>
2023-02-19 14:42:41 +0000

    finish

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-var form = document.getElementById("my-form");
 var nameError = document.getElementById("name-error");
 var emailError = document.getElementById("email-error");
 var phoneError = document.getElementById("phone-error");
 var messageError = document.getElementById("message-error");
 var submitError = document.getElementById("submit-error");
-
 
 function validateName(){
     var name = document.getElementById("contact-name").value;
@@ -66,59 +64,3 @@ function validateMessage(){
     return true;
 }
 
-function validateForm() {
-  var isNameValid = validateName();
-  var isPhoneValid = validatePhone();
-  var isEmailValid = validateEmail();
-  var isMessageValid = validateMessage();
-  
-  return isNameValid && isPhoneValid && isEmailValid && isMessageValid;
-}
-
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("submit-button");
-
-      if (!validateForm()) {
-        submitError.innerHTML = "Por favor, preencha todos os campos corretamente.";
-        return;
-      } else {
-        nameError.innerHTML = "";
-        emailError.innerHTML = "";
-        phoneError.innerHTML = "";
-        messageError.innerHTML = "";
-        submitError.innerHTML = "";
-      }
-
-      
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.style.color = "green";
-          status.innerHTML = "Mensagem enviada!";
-          status.classList.add("success"); // adiciona classe "success" ao elemento de status
-          form.reset();
-          setTimeout(function() {
-            status.innerHTML = "Enviar mensagem"; // remove o texto do elemento de status
-            status.style.color = "";
-          }, 3000);
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
